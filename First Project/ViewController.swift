6446a7aaeda0a3528381d33448180274d7700ab4
manolomanatlao <manolo.manatlao@gmail.com>
2021-01-29 00:23:29 +0400

    Initial Commit

---
+++ First Project/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  First Project
+//
+//  Created by Deejay Manolito on 1/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    /**
+     Adds two numbers together and returns the result.
+     - parameter num1: The firtst number
+     - parameter num2: The second number
+     - returns: The sum of num1 and num2
+     */
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    
+
+}
+
